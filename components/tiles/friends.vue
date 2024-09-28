cd9b72b4af08718ebf46f81e7de60b1a257ef7e4
vee <vee@softmodded.com>
2024-09-28 13:27:26 -0400

    finish homepage

--- components/tiles/friends.vue
+++ components/tiles/friends.vue
@@ -12,11 +12,14 @@ function push(link) {
       class="border-2 border-gray-200 rounded-md w-[620px] h-[195px] text-center mb-5"
     >
       <div class="flex mb-2">
-        <Icon name="material-symbols:notifications-outline" class="w-6 h-6 mt-3 ml-3" />
+        <Icon
+          name="material-symbols:notifications-outline"
+          class="w-6 h-6 mt-3 ml-3"
+        />
         <h1 class="ml-1 mt-2 text-left font-semibold text-lg">notifications</h1>
       </div>
       <div
-        class="w-full h-[40px] flex border-t-2 border-gray-200  cursor-pointer hover:bg-gray-100 transition-all"
+        class="w-full h-[40px] flex border-t-2 border-gray-200 cursor-pointer hover:bg-gray-100 transition-all"
       >
         <p class="font-bold text-lg w-1/4 text-left ml-2 mt-0.5 truncate mr-4">
           Price drop alert!
@@ -27,7 +30,7 @@ function push(link) {
         </p>
       </div>
       <div
-        class="w-full h-[40px] flex border-t-2 border-gray-200  cursor-pointer hover:bg-gray-100 transition-all"
+        class="w-full h-[40px] flex border-t-2 border-gray-200 cursor-pointer hover:bg-gray-100 transition-all"
       >
         <p class="font-bold text-lg w-1/4 text-left ml-2 mt-0.5 truncate mr-4">
           Price drop alert!
@@ -38,7 +41,7 @@ function push(link) {
         </p>
       </div>
       <div
-        class="w-full h-[40px] flex border-t-2 border-gray-200 border-b-2  cursor-pointer hover:bg-gray-100 transition-all"
+        class="w-full h-[40px] flex border-t-2 border-gray-200 border-b-2 cursor-pointer hover:bg-gray-100 transition-all"
       >
         <p class="font-bold text-lg w-1/4 text-left ml-2 mt-0.5 truncate mr-4">
           Price drop alert!
@@ -48,16 +51,47 @@ function push(link) {
           threshold of <strong>$21.99</strong>
         </p>
       </div>
-      <a href="#" @click="push('/updates/2024-9-28')" class="underline">see more →</a>
+      <a href="#" @click="push('/updates/2024-9-28')" class="underline"
+        >see more →</a
+      >
     </div>
 
-
-
-
     <div
-      class="mt-4 border-2 border-gray-200 rounded-md w-[620px] h-[195px] text-center"
+      class="mt-4 border-2 border-gray-200 rounded-md w-[620px] h-[195px] flex justify-center align-middle text-center"
     >
-      pending requests
+      <div class="w-[200px] flex justify-center align-middle cursor-pointer transition-all hover:scale-105">
+        <div>
+          <img
+            src="https://skylanderscharacterlist.com/wp-content/uploads/2013/02/Zap.png"
+            alt="zap"
+            class="w-[120px] mt-3 mx-auto"
+          />
+          <p class="font-bold mt-1">Zap</p>
+          <p>Current price: <strong>$3.99</strong></p>
+        </div>
+      </div>
+      <div class="w-[200px] flex justify-center align-middle cursor-pointer transition-all hover:scale-105">
+        <div>
+          <img
+            src="https://skylanderscharacterlist.com/wp-content/uploads/2013/02/Zap.png"
+            alt="zap"
+            class="w-[120px] mt-3 mx-auto"
+          />
+          <p class="font-bold mt-1">Zap</p>
+          <p>Current price: <strong>$3.99</strong></p>
+        </div>
+      </div>
+      <div class="w-[200px] flex justify-center align-middle cursor-pointer transition-all hover:scale-105">
+        <div>
+          <img
+            src="https://skylanderscharacterlist.com/wp-content/uploads/2013/02/Zap.png"
+            alt="zap"
+            class="w-[120px] mt-3 mx-auto"
+          />
+          <p class="font-bold mt-1">Zap</p>
+          <p>Current price: <strong>$3.99</strong></p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
